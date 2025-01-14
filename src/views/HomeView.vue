87e2c8a6a221a2e8494175ff09602b00545fccb0
AranyosiDanielKeri <aranyosi.daniel-2020@keri.mako.hu>
2025-01-14 08:45:51 +0100

    quick main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 //import TheWelcome from '../components/TheWelcome.vue'
 import PageHeader from '../components/PageHeader.vue';
+import PageMain from '../components/PageMain.vue';
 </script>
 
 <template>
     <main>
-        <PageHeader />
+        <PageHeader :link-count="3">
+
+        </PageHeader>
 
 
-        <PageMain />
+        <PageMain>
+            <div class="zsirke">
+                balls
+            </div>
+        </PageMain>
 
 
 
