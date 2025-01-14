63cfafce4c462d80b153ed4cc3440cd6f664fc7c
szelladamkeri <szell.adam-2020+1@keri.mako.hu>
2025-01-14 11:56:36 +0100

    home update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,19 @@ import PageHeader from '../components/PageHeader.vue';
 </script>
 
 <template>
-    <main>
+    <main class="relative">
         <PageHeader />
 
+        <div class="w-[93.6rem] ml-[7.2rem] h-[46rem] bg-black bg-opacity-70 backdrop-blur-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
 
         <PageMain />
-
-
 
     </main>
 </template>
 
 <style>
+main {
+    position: relative;
+}
 #trending {}
 </style>
