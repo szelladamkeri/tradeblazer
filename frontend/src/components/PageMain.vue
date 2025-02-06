ac034292cf2a652e2c3596b03d61a332439ae472
AranyosiDanielKeri <aranyosi.daniel-2020@keri.mako.hu>
2025-02-06 07:44:27 +0100

    same

--- frontend/src/components/PageMain.vue
+++ frontend/src/components/PageMain.vue
@@ -23,16 +23,4 @@ h3 {
     font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
-    text-align: center;
-}
-
-@media (min-width: 1024px) {
-
-    .greetings h1,
-    .greetings h3 {
-        text-align: left;
-    }
-}
 </style>
