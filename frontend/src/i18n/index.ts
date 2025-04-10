c5eca93ba6cf61d6c2895c458b321688d299bb4d
szelladamkeri <124137959+szelladamkeri@users.noreply.github.com>
2025-04-10 14:40:06 +0200

    translation finished

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -2,12 +2,14 @@ import { createI18n } from 'vue-i18n'
 import en from '../locales/en.json'
 import hu from '../locales/hu.json'
 
-// Get the saved language from localStorage or default to English
+/**
+ * Retrieve user language preference from localStorage or default to English
+ */
 const savedLanguage = localStorage.getItem('language') || 'en'
 
 export default createI18n({
-  legacy: false, // Use Composition API
-  locale: savedLanguage, // Use the saved language
+  legacy: false,
+  locale: savedLanguage,
   fallbackLocale: 'en',
   messages: {
     en,
